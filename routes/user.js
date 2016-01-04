f0a0eb2dcb15aee67f805024e314c07318c792de
bassfan <fanfanyayu@gmail.com>
2016-01-04 15:46:18 +0800

    前端第一版完成

--- routes/user.js
+++ routes/user.js
@@ -7,19 +7,29 @@ user.get('/:id',function(req,res){
 	res.render('./user/user',{
     title:'个人中心',
     personal:{
-      avt:'',
-      _id:req.params.id,
-      data:{
-        name:'fanfan',
-        age:18,
-        registTime:'2015-12-30',
-        touch:'18569422761'
-      },
-      favorite:{
-        a:'aaaa',
-        b:'bbbbb'
+        avt:'',
+        _id:req.params.id,
+        data:{
+          name:'fanfan',
+          age:18,
+          registTime:'2015-12-30',
+          touch:'18569422761'
+        },
+        favorite:{
+          dramas:{
+            drama1:{
+              _id:5,
+              poster:'',
+              name:'一公升的眼泪'
+            },
+            drama2:{
+              _id:6,
+              poster:'',
+              name:'二公升的眼泪'
+            }
+          }
+        }
       }
-    }
   });
 
   console.log(req.params.id);
