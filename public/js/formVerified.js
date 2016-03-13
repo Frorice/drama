a61913a1b1e8b58890bbfaba3c650ce79cfe75e4
bassfan <fanfanyayu@gmail.com>
2016-03-13 20:18:38 +0800

    莫名的表单验证

---
+++ public/js/formVerified.js
@@ -0,0 +1,22 @@
+
+document.getElementById('submit').onclick = formSubmit;
+
+function formSubmit(){
+  // var formItems = document.getElementsByTagName('form')[1].getElementsByTagName('input');
+
+  // for(var i in formItems){
+  //   console.log(i)
+  //   if(formItems[i].value === ''){
+  //     formItems[i].value = '输入值不能为空！';
+  //     formItems[i].focus();
+  //     return false;
+  //   }
+  //   if(formItems[i].type === formItems[i+1].type === 'password'){
+  //     if(formItems[i].value!==formItems[i+1].value){
+  //       alert('密码不一致！');
+  //     }
+  //     return false;
+  //   }
+  // }
+  //document.getElementsByTagName('form')[0].submit();
+};
