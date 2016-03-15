d1c0dcd53a24d15e0cf0af8f3224ba8a13022a1b
bassfan <fanfanyayu@gmail.com>
2016-03-15 19:52:58 +0800

    完善电视剧搜索

--- controllers/root.js
+++ controllers/root.js
@@ -41,47 +41,21 @@ exports.admin = function(req,res){
 }
 // search page
 exports.search = function(req, res) {
-  var catId = req.query.cat
   var q = req.query.q
   var page = parseInt(req.query.p, 10) || 0
   var count = 2
   var index = page * count
 
-  if (catId) {
-    Category
-      .find({_id: catId})
-      .populate({
-        path: 'drama',
-        select: 'name poster'
-      })
-      .exec(function(err, categories) {
-        if (err) {
-          console.log(err)
-        }
-        var category = categories[0] || {}
-        var dramas = category.dramas || []
-        var results = dramas.slice(index, index + count)
-
-        res.render('results', {
-          title: '搜索结果',
-          keyword: category.name,
-          currentPage: (page + 1),
-          query: 'cat=' + catId,
-          totalPage: Math.ceil(dramas.length / count),
-          dramas: results
-        })
-      })
-  }
-  else {
+  
     Drama
-      .find({title: new RegExp(q + '.*', 'i')})
+      .find({name: new RegExp(q + '.*', 'i')})
       .exec(function(err, dramas) {
         if (err) {
           console.log(err)
         }
         var results = dramas.slice(index, index + count)
 
-        res.render('results', {
+        res.render('./results', {
           title: '搜索结果',
           keyword: q,
           currentPage: (page + 1),
@@ -91,4 +65,3 @@ exports.search = function(req, res) {
         })
       })
   }
-}
